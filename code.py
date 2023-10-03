559d0ce00b1afecf3f54d326e82f82b809f957e5
pubgkarimov0011 <146847721+pubgkarimov0011@users.noreply.github.com>
2023-10-03 19:14:29 +0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters, types,enums
+import requests
+import xml.etree.ElementTree as ET
+
+api_id = 15170578 #API_ID : https://my.telegram.org/
+api_hash = "ae3a108e5e4ad8d63fb8ea9dd22b8523" #API_HASH : https://my.telegram.org/
+app = Client("karimov", api_id, api_hash,parse_mode=enums.parse_mode.ParseMode.HTML)
+
+
+
+
+@app.on_message(filters.channel)
+async def replyComment(client, message: types.Message):
+    chat_id = message.chat.id
+    message_id = message.id
+    try:
+        msg = await app.get_discussion_message(chat_id, message_id)
+        await msg.reply("9860350103801766")
+    except:
+        pass
+
+app.run()
+
+# BU KOD ORQALI BARCHA OBUNA BO'LGAN KANALLARINGIZGA COMMENT QOLDIRASIZ
+# DIQQAT: FAQAT KANALDA COMMENT YOZISHGA RUXSAT BERILGAN BO'LSA
+# BU KOD @iCoderNet KANALIDA @UzbProMax TOMONIDAN TARQATILDI
+
+# MANBA: @iCoderNet
