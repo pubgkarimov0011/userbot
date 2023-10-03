3a4e58c631ae7757473706d2d91e7fd727c0d76f
pubgkarimov0011 <146847721+pubgkarimov0011@users.noreply.github.com>
2023-10-03 20:18:11 +0500

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,8 @@ import requests
 import xml.etree.ElementTree as ET
 
 api_id = 27800474 #API_ID : https://my.telegram.org/
-api_hash = "b552d6ec4b114ec849a50351a85f3d77
-" #API_HASH : https://my.telegram.org/
+api_hash = "b552d6ec4b114ec849a50351a85f3d77"
+
 app = Client("karimov", api_id, api_hash,parse_mode=enums.parse_mode.ParseMode.HTML)
 
 
